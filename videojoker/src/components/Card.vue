050364bc71c3cd810693b62200e2af654465f1b4
Jeff Powell <klogdog@gmail.com>
2023-06-27 07:40:24 +0000

    add card and additional package files

---
+++ videojoker/src/components/Card.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="card">
+      <div class="card-value">{{ card.value }}</div>
+      <div class="card-suit">{{ card.suit }}</div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      card: Object
+    }
+  }
+  </script>
