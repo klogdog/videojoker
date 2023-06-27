de3ac9e0829e8ea7ee0b994f5c7a349e5c32a50d
Jeff Powell <klogdog@gmail.com>
2023-06-27 07:55:24 +0000

    Update card to contain face card checking

--- videojoker/src/components/Card.vue
+++ videojoker/src/components/Card.vue
@@ -1,11 +1,12 @@
 <template>
     <div class="card">
       <div class="card-value">{{ card.value }}</div>
+      <img v-if="isFaceCard" class="card-face" :src="faceCardImage" alt="Face Card">
       <div class="card-suit">{{ card.suit }}</div>
     </div>
-  </template>
+</template>
   
-  <script>
+<script>
   export default {
     name: 'Card',
     props: {
@@ -13,12 +14,24 @@
         type: Object,
         required: true
       }
+    },
+    computed: {
+      isFaceCard() {
+        return ['J', 'Q', 'K', 'A'].includes(this.card.value);
+      },
+      faceCardImage() {
+        if (this.isFaceCard) {
+          return `/path/to/${this.card.value}-face-card.png`;
+        }
+        return '';
+      }
     }
   }
   </script>
   
   <style scoped>
   .card {
+    position: relative;
     width: 100px;
     height: 150px;
     border: 1px solid black;
@@ -31,7 +44,18 @@
   }
   
   .card-value {
+    position: absolute;
+    top: 5px;
+    left: 5px;
     font-size: 20px;
+  }
+  
+  .card-face {
+    position: absolute;
+    top: 5px;
+    right: 5px;
+    width: 30px;
+    height: 30px;
   }
   
   .card-suit {
