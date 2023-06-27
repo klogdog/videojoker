87957b43c9edf867bddfe858b31695e1de3bd0fd
Jeff Powell <klogdog@gmail.com>
2023-06-27 07:46:10 +0000

    Update Card

--- videojoker/src/components/Card.vue
+++ videojoker/src/components/Card.vue
@@ -7,8 +7,35 @@
   
   <script>
   export default {
+    name: 'Card',
     props: {
-      card: Object
+      card: {
+        type: Object,
+        required: true
+      }
     }
   }
   </script>
+  
+  <style scoped>
+  .card {
+    width: 100px;
+    height: 150px;
+    border: 1px solid black;
+    border-radius: 5px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin: 5px;
+  }
+  
+  .card-value {
+    font-size: 20px;
+  }
+  
+  .card-suit {
+    font-size: 15px;
+  }
+  </style>
+  
