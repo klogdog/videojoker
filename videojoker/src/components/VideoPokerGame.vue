260c897f5419df6dbcbd09c2356926e25629e362
Jeff Powell <klogdog@gmail.com>
2023-06-29 06:22:27 +0000

    Working Jacks or Better code

--- videojoker/src/components/VideoPokerGame.vue
+++ videojoker/src/components/VideoPokerGame.vue
@@ -4,7 +4,7 @@
     <button class="control-button" @click="handleButtonClick">{{ buttonText }}</button>
     <card-deck ref="cardDeck"></card-deck>
     <div v-if="gameOver">
-      <h1>Game Over</h1>
+      <h1>{{ score }}</h1>
     </div>
   </div>
 </template>
@@ -21,10 +21,11 @@ export default {
   },
   data() {
     return {
-      hand: Array(5).fill({ suit: undefined, value: undefined }), // Initializing hand with placeholder cards
+      hand: Array(5).fill({ suit: undefined, value: undefined }), // Initializing hand with placeholder cards,
       heldCards: Array(5).fill(false),
       gameState: 'start',  // can be 'start', 'draw', 'gameOver'
-      gameOver: false
+      gameOver: false,
+      score: ''
     }
   },
   computed: {
@@ -34,7 +35,6 @@ export default {
   },
   methods: {
     startGame() {
-      this.$refs.cardDeck.prepareDeck();
       this.hand = this.$refs.cardDeck.draw(5);
       this.gameState = 'draw';
     },
@@ -53,15 +53,82 @@ export default {
         }
       }
       this.heldCards = Array(5).fill(false);
-      this.scoreHand();
-      this.gameOver = true; // Mark the game as over after scoring the hand
+      this.score = this.scoreHand();
+      this.gameOver = true;
       this.gameState = 'gameOver';
     },
     scoreHand() {
-      console.log("Scoring hand: ", this.hand);
-    },
+  let suits = this.hand.map(card => card.suit);
+  let values = this.hand.map(card => card.value);
+  values.sort((a, b) => {
+    if (['J', 'Q', 'K', 'A'].includes(a)) a = 11 + ['J', 'Q', 'K', 'A'].indexOf(a);
+    if (['J', 'Q', 'K', 'A'].includes(b)) b = 11 + ['J', 'Q', 'K', 'A'].indexOf(b);
+    return a - b;
+  });
+  
+  let uniqueSuits = [...new Set(suits)];
+  let uniqueValues = [...new Set(values)];
+  
+  // Count occurrences of each card value
+  let counts = values.reduce((acc, val) => {
+    if (val in acc) {
+      acc[val]++;
+    } else {
+      acc[val] = 1;
+    }
+    return acc;
+  }, {});
+
+  // Check for Royal Flush
+  if (uniqueSuits.length === 1 && values.join('') === '10JQKA') {
+    return 'Royal Flush';
+  }
+
+  // Check for Straight Flush
+  if (uniqueSuits.length === 1 && uniqueValues.length === 5 && values[4] - values[0] === 4) {
+    return 'Straight Flush';
+  }
+
+  // Check for Four of a Kind
+  if (Object.values(counts).includes(4)) {
+    return 'Four of a Kind';
+  }
+
+  // Check for Full House
+  if (Object.values(counts).includes(3) && Object.values(counts).includes(2)) {
+    return 'Full House';
+  }
+
+  // Check for Flush
+  if (uniqueSuits.length === 1) {
+    return 'Flush';
+  }
+
+  // Check for Straight
+  if (uniqueValues.length === 5 && values[4] - values[0] === 4) {
+    return 'Straight';
+  }
+
+  // Check for Three of a Kind
+  if (Object.values(counts).includes(3)) {
+    return 'Three of a Kind';
+  }
+
+  // Check for Two Pair
+  if (Object.values(counts).filter(val => val === 2).length === 2) {
+    return 'Two Pair';
+  }
+
+  // Check for Jacks or Better
+  if (Object.entries(counts).filter(([key, val]) => ['J', 'Q', 'K', 'A'].includes(key) && val >= 2).length > 0) {
+    return 'Jacks or Better';
+  }
+
+  return 'No Match';
+},
     restartGame() {
-      this.hand = Array(5).fill({ suit: undefined, value: undefined }); // Fill the hand with placeholder cards
+      this.$refs.cardDeck.prepareDeck();
+      // this.hand = Array(5).fill({ suit: undefined, value: undefined }); // Initializing hand with placeholder cards
       this.heldCards = Array(5).fill(false);
       this.gameState = 'start';
       this.gameOver = false;
@@ -75,6 +142,9 @@ export default {
         this.restartGame();
       }
     }
+  },
+  mounted() {
+    this.restartGame();
   }
 }
 </script>
